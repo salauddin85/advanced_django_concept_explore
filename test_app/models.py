c8e66346c82987487ff5d1add55187ba02eabf0b
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-05 08:39:11 +0600

    added custom lookup field in django

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models import Lookup, CharField
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+# Custom Lookup
+@CharField.register_lookup
+class IsUpper(Lookup):
+    lookup_name = 'isupper'
+
+    def as_sql(self, compiler, connection):
+        lhs_sql, lhs_params = self.process_lhs(compiler, connection)
+        if self.rhs:
+            return f"UPPER({lhs_sql}) = {lhs_sql}", lhs_params * 2
+        else:
+            return f"UPPER({lhs_sql}) != {lhs_sql}", lhs_params * 2
--- test_app/views.py
+++ test_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductListView(APIView):
+    def get(self, request):
+        products = Product.objects.filter(name__isupper=True)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
