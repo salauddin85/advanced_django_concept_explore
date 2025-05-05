1a00d67a8dad7e3143ce3247d518b4df3e1fb71a
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-05 11:08:18 +0600

    added locust in project

--- test_app/views.py
+++ test_app/views.py
@@ -10,3 +10,32 @@ class ProductListView(APIView):
         products = Product.objects.filter(name__isupper=True)
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+
+from rest_framework import viewsets
+from .models import Blog
+from .serializers import BlogSerializer
+from rest_framework.permissions import AllowAny
+
+class BlogViewSet(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+    
+
+
+
+# output:
+[
+    {
+        "id": 1,
+        "name": "SHIRT"
+    },
+    {
+        "id": 3,
+        "name": "PANTS"
+    }
+]
+# Salauddin
