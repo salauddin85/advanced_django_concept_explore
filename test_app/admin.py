f68542b1da33f30def20b59be5048e9855f69888
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-09 23:40:53 +0600

    change admin.py

--- test_app/admin.py
+++ test_app/admin.py
@@ -16,7 +16,13 @@ app_models = apps.get_app_config('test_app').get_models()
 
 for model in app_models:
     class DynamicAdmin(admin.ModelAdmin):
-        list_display = [field.name for field in model._meta.fields]  # সব ফিল্ড দেখাবে
+        list_display = [field.name for field in model._meta.fields]  
         search_fields = [field.name for field in model._meta.fields if field.__class__.__name__ == 'CharField']  # কেবল string ফিল্ডের জন্য সার্চ
-
-    admin.site.register(model, DynamicAdmin)
+        list_filter = [field.name for field in model._meta.fields if field.__class__.__name__ == 'BooleanField']
+        ordering = [field.name for field in model._meta.fields if field.__class__.__name__ == 'DateTimeField']
+        prepopulated_fields = {"slug": ("name",)}
+        
+    try:
+        admin.site.register(model, DynamicAdmin)
+    except admin.sites.AlreadyRegistered:
+        pass
