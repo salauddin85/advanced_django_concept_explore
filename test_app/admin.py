f9a80298602eab52d2313c4aebbb9c8b2abaa034
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-09 23:20:40 +0600

    added all model register in admin.py

--- test_app/admin.py
+++ test_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from django.apps import apps
 # Register your models here.
+apps_models = apps.get_app_config('test_app').get_models()
+for model in apps_models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
